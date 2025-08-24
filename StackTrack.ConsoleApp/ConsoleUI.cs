bce0d163d4a39d51fae4b1969a6188d2165df94d
FreddyJ01 <fahrodej01@gmail.com>
2025-08-23 21:32:00 -0500

    Planning

--- StackTrack.ConsoleApp/ConsoleUI.cs
+++ StackTrack.ConsoleApp/ConsoleUI.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using Accounts;
 namespace ConsoleUI;
 
@@ -55,5 +56,36 @@ class HomeScreen
             userInput = Console.ReadLine();
             int.TryParse(userInput, out userChoice);
         } while (userInput != "exit");
+    }
+
+    void HomeScreenLogic(int userSelection)
+    {
+        switch (userSelection)
+        {
+            case 1:
+                // Call to DisplayBooks() -> Display Strings of Available books by genre
+                // Take user input to allow a user to select a book -> allow the user to either be done or check out more books (recursive call)
+                // WHen a user checks out a book we need to log the time and give them a message telling them the date when they need to return the book by
+                break;
+            case 2:
+                // Call to RetrunBooks() -> Display current stack of books
+                // Take user input to allow them to return a specific book
+                // Handle any late fees that the user might have and take them to a payment page or allow them to add it to their balance
+                // Ask if they have more returns if not back to the home screen if they do then recursion
+                break;
+            case 3:
+                // Shows the user their current stack of books and allow them to select a book and see how long they've had it checked out.
+                // Maybe down the road allow users to take notes on the book and acces their notes on specific books, this would mean storing historical checkouts so they dont lose their notes when they return a book
+                break;
+            case 4:
+                // Show the user their current balance and allow them to make a payment
+                break;
+            case 5:
+                // Shows the user our late fee terms and other policies
+                break;
+            case 6:
+                // Logs the user out and takes them back to the Main Menu
+                break;
+        }
     }
 }
